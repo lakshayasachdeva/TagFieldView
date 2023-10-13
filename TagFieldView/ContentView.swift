0c9d638ad971d6ee1681d60e827f263cf38fcc45
Lakshaya Sachdeva <lakshaya.sachdeva@colearn.id>
2023-10-13 13:24:19 +0530

    Initial Commit

---
+++ TagFieldView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TagFieldView
+//
+//  Created by Lakshaya Sachdeva on 13/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TagFieldView/TagFieldViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagFieldViewApp.swift
+//  TagFieldView
+//
+//  Created by Lakshaya Sachdeva on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TagFieldViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
